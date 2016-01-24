c1d2a7d26d74b3dd7092347a0f693a3045f9735d
worgflores <worgflores@gmail.com>
2016-01-25 01:16:52 +0800

    rameses-etracs-main: - added PIN to the address page

--- client/etracs/rameses-etracs-main/src/com/rameses/etracs/shared/ReportController.groovy
+++ client/etracs/rameses-etracs-main/src/com/rameses/etracs/shared/ReportController.groovy
@@ -4,136 +4,23 @@ import com.rameses.rcp.common.*;
 import com.rameses.rcp.annotations.*;
 import com.rameses.osiris2.client.*;
 import com.rameses.osiris2.reports.*;
-import java.io.*;
-import java.net.URL;
 
-abstract class ReportController
-{
-    def clientContext = com.rameses.rcp.framework.ClientContext.currentContext;
-
-    @Binding
-    def binding
-    
-    @Service("ReportParameterService")
-    def svcParams;
-    
+public abstract class ReportController extends com.rameses.osiris2.reports.ReportController 
+{ 
     @Service('DateService')
-    def dtSvc
-    
-    def mode
-    def entity = [:];
-    def params = [:];
-    def reportdata
-    
-    abstract def getReportData();
-    abstract String getReportName();
-    
-    
-    
-    def getFormControl(){
-        return null;
-    }
-    
-    
-    SubReport[] getSubReports(){ 
-        return null;
-    }
-    
-    Map getParameters(){
-        return [:]
-    }
-
-    public String getImagePath( def imagename ) {
-        def appEnv = clientContext.appEnv; 
-        def customfolder = appEnv['report.custom'];
-        if (!customfolder) customfolder = appEnv['app.custom'];
+    def dtSvc; 
         
-        def path = 'images/' + imagename 
-        if( customfolder ) { 
-            def cpath = 'images/' + customfolder + '/' + imagename  
-            if( clientContext.getResource(cpath) )  path = cpath 
-        } 
-
-        return path 
-    }
-    
-    public URL getURL( def imagename ) { 
-        try { 
-            return clientContext.classLoader.getResource( getImagePath( imagename ) ); 
-        } catch(e) {
-            return null; 
-        }
-    }
-    
-    public InputStream getInputStream( def imagename ) {
-        return clientContext.classLoader.getResourceAsStream( getImagePath( imagename ) );
-    }
-
-    public def getResource( def imagename) {
-        def path = getImagePath( imagename );
-        if(!path) return null
-        
-        def url = clientContext.classLoader.getResource( path )
-        return url ? url.toString() : null ;
-    }
-    
-    def initReport(){
-        return 'default'
-    }
-    
     def init() {
         def parsedate = dtSvc.parseCurrentDate();
+        entity.month = getMonthsByQtr().find{it.index == parsedate.month} 
         entity.year = parsedate.year;
         entity.qtr  = parsedate.qtr;
-        entity.month = getMonthsByQtr().find{it.index == parsedate.month}
         entity.day = parsedate.day;
-        mode = 'init'
-        return initReport();
-    }
-    
-    def preview() {
-        buildReport()
-        mode = 'view'
-        return 'preview' 
-    }
-    
-    
-    void print(){
-        buildReport()
-        ReportUtil.print( report.report, true )
-    }
-    
-    void buildReport(){
-        reportdata = getReportData();
-        params = svcParams.getStandardParameter()  + getParameters() 
-
-        def loaders = Inv.lookupOpeners('report-parameter-loader', [ params : params ]); 
-        loaders.each{ o-> 
-            try {
-                o.target = 'process'; 
-                Inv.invokeOpener( o );  
-            } catch (Throwable t) { 
-                t.printStackTrace(); 
-            } 
-        } 
-
-        report.viewReport()
-    }
-        
-    def report = [
-        getReportName : { return getReportName() }, 
-        getReportData : { return reportdata },
-        getSubReports : { return getSubReports() },
-        getParameters : { return  params },
-    ] as ReportModel;
-    
-    def back() {
-        mode = 'init'
-        return 'default' 
+        return super.init(); 
     }
     
     List getQuarters() {
-        return [1,2,3,4]
+        return [1,2,3,4]; 
     }
         
     List getMonthsByQtr() {
@@ -143,5 +30,4 @@ abstract class ReportController
     List getMonths(){
         return getMonthsByQtr();
     }
-    
 }

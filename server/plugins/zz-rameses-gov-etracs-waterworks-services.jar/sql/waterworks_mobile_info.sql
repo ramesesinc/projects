9b9d370f823b17ac4434f04c9e7c73796bcf89d0
dinoquimson <dinoquimson1992@gmail.com>
2016-02-12 18:03:47 -0800

    updates of zz-rameses-gov-etracs-waterworks-services.jar

--- server/plugins/zz-rameses-gov-etracs-waterworks-services.jar/sql/waterworks_mobile_info.sql
+++ server/plugins/zz-rameses-gov-etracs-waterworks-services.jar/sql/waterworks_mobile_info.sql
@@ -6,11 +6,6 @@ AND wa.areaid IN (${areaids})
 AND lastreadingyear = $P{year}
 AND lastreadingmonth = $P{month}
 
-[findBatchCount]
-SELECT count(*) as batchcount
-FROM waterworks_mobile_info
-WHERE batchid = $P{batchid}
-
 [getListForDownload]
 SELECT 
 wa.objid, wa.acctno, wa.acctname, wa.address_text AS address, wa.mobileno, wa.phoneno, wa.email, wm.serialno, wa.areaid,
@@ -21,14 +16,3 @@ INNER JOIN waterworks_meter wm ON wa.meterid = wm.objid
 INNER JOIN waterworks_area war ON wa.areaid = war.objid
 INNER JOIN waterworks_mobile_info wd ON wd.objid = wa.objid
 WHERE wd.batchid = $P{batchid}
-
-[confirmDownload]
-UPDATE waterworks_mobile_info 
-SET state = 'DOWNLOADED'
-WHERE batchid = $P{batchid}
-
-[deleteDownload]
-DELETE FROM waterworks_mobile_info
-WHERE batchid = $P{batchid}
-AND objid = $P{acctid}
-AND state = 'DOWNLOADED'

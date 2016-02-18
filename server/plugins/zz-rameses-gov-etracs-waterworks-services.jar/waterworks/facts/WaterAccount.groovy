b2ed8ba696ab3616af6f641d6b9341ac41259a05
Elmo Nazareno <elmonazareno@gmail.com>
2016-02-18 18:02:23 +0800

    updated WaterAccount fact

--- server/plugins/zz-rameses-gov-etracs-waterworks-services.jar/waterworks/facts/WaterAccount.groovy
+++ server/plugins/zz-rameses-gov-etracs-waterworks-services.jar/waterworks/facts/WaterAccount.groovy
@@ -6,10 +6,13 @@ import treasury.facts.*;
 public class WaterAccount {
 
     String classification;
-	
-	public WaterAccount(def app) {
-		this.classification = app.classification;
-	}
+    String metersize;	
+    String barangay;
 
+	public WaterAccount( def app ) {
+		this.classification = app.classificationid;
+		this.metersize = app.meter?.sizeid;
+		this.barangay = app.barangay?.objid;
+	}
 
 }

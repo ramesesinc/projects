3d667bba4ce7b1ccc13cdbd8eae4c02675bb044d
dinoquimson <dinoquimson1992@gmail.com>
2016-03-29 10:47:57 +0800

    updates of waterworks-mobile

--- android/waterworks-mobile/src/main/java/com/rameses/waterworks/bean/ItemAccount.java
+++ android/waterworks-mobile/src/main/java/com/rameses/waterworks/bean/ItemAccount.java
@@ -1,5 +1,7 @@
 package com.rameses.waterworks.bean;
 
+import java.text.DecimalFormat;
+
 public class ItemAccount {
     
     private String account;
@@ -11,6 +13,20 @@ public class ItemAccount {
     }
     
     public String getAccount(){ return account; }
-    public double getAmount(){ return amount; }
+    
+    public String getAmount(){ 
+        return format(amount);
+    }
+    
+    public static String format(double amount){
+        DecimalFormat df = new DecimalFormat("0.00");
+        String val = df.format(amount);
+        int strlen = val.length();
+        String prefix = "";
+        for(int x=strlen; x <= 5; x++){
+            prefix += " ";
+        }
+        return prefix + val; 
+    }
     
 }

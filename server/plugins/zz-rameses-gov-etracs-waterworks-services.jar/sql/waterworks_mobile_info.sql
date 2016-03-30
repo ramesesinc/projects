e3d746897c42699512fea2fed2e4c5a894bfeb78
dinoquimson <dinoquimson1992@gmail.com>
2016-03-30 15:17:19 +0800

    updates of zz-rameses-gov-etracs-waterworks-services.jar

--- server/plugins/zz-rameses-gov-etracs-waterworks-services.jar/sql/waterworks_mobile_info.sql
+++ server/plugins/zz-rameses-gov-etracs-waterworks-services.jar/sql/waterworks_mobile_info.sql
@@ -1,17 +1,17 @@
-[getDownloadableAccount]
+[getListForDownload]
 SELECT 
 wa.objid, wa.acctno, wa.acctname, wa.address_text as address, wa.mobileno,
 wm.serialno, wm.sizeid as metersize, wr.objid as areaid, wa.lastreading, 
 wa.lasttxndate, wr.title as areaname, wa.classificationid, wa.lastreadingyear,
-wa.lastreadingmonth, wa.lastreadingdate, wa.address_barangay_objid as barangayid
+wa.lastreadingmonth, wa.lastreadingdate, wa.address_barangay_objid as barangayid,
+wsa.parentid as stuboutid, wsa.sortorder
 FROM waterworks_account wa
 INNER JOIN waterworks_meter wm ON wa.meterid = wm.objid
 INNER JOIN waterworks_stubout_account wsa ON wsa.acctid = wa.objid
 INNER JOIN waterworks_stubout ws on ws.objid = wsa.parentid
 INNER JOIN waterworks_area wr on wr.objid = ws.areaid
 WHERE wa.objid NOT IN (SELECT objid FROM waterworks_mobile_info)
-AND wr.assignee_objid = $P{assigneeid}
-AND wr.objid IN ${groupids}
+AND wr.objid = $P{areaid}
 
 [cancelDownload]
 DELETE FROM waterworks_mobile_info

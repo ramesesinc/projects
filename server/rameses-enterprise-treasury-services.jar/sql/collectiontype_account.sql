24955b5dd5dc992624c8c5b3e6e3fdab46f6dc9f
worgflores <worgflores@gmail.com>
2016-03-30 15:26:22 +0800

    TREASURY: updated collectiontype_account.sql

--- server/rameses-enterprise-treasury-services.jar/sql/collectiontype_account.sql
+++ server/rameses-enterprise-treasury-services.jar/sql/collectiontype_account.sql
@@ -21,3 +21,12 @@ ri.fund_title AS account_fund_title, ri.fund_code AS account_fund_code
 FROM collectiontype_account ca
 INNER JOIN itemaccount ri ON ri.objid=ca.account_objid
 WHERE ca.collectiontypeid=$P{objid} AND ca.tag=$P{tag} 
+
+[findAccountByHandlerAndTag]
+SELECT cta.*,  
+	ia.code AS account_code, ia.fund_objid AS account_fund_objid,
+	ia.fund_title AS account_fund_title, ia.fund_code AS account_fund_code 
+FROM collectiontype ct, collectiontype_account cta, itemaccount ia 
+WHERE ct.handler=$P{handler} and cta.tag=$P{tag} 
+	AND ct.objid=cta.collectiontypeid 
+	AND cta.account_objid=ia.objid 

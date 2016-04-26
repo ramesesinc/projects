a4365d7960db70bc67b2c97d8d9a5ee565908277
worgflores <worgflores@gmail.com>
2016-04-26 11:45:26 +0800

    PLUGIN: WATERWORKS: updated services

--- server/plugins/zz-rameses-gov-etracs-waterworks-services.jar/sql/waterworks_account.sql
+++ server/plugins/zz-rameses-gov-etracs-waterworks-services.jar/sql/waterworks_account.sql
@@ -1,4 +1,14 @@
 [clearStubout]
 UPDATE waterworks_account SET 
 	stuboutid=NULL, stuboutindex=NULL 
-WHERE objid=$P{objid} 
+WHERE 
+	objid=$P{objid} 
+
+
+[updateReading]
+UPDATE waterworks_account SET 
+	lastdateread=$P{lastdateread}, 
+	currentreading=$P{currentreading}, 
+	billingcycleid=$P{billingcycleid} 
+WHERE 
+	objid=$P{objid} 
--- server/plugins/zz-rameses-gov-etracs-waterworks-services.jar/sql/waterworks_mobile_info.sql
+++ server/plugins/zz-rameses-gov-etracs-waterworks-services.jar/sql/waterworks_mobile_info.sql
@@ -3,9 +3,10 @@ SELECT
 	acct.objid, acct.acctno, acct.acctname, acct.address_text AS address,
 	wm.serialno, wm.sizeid AS metersize, s.objid AS sectorid, s.code AS sectorcode, 
 	acct.classificationid, acct.address_barangay_objid AS barangayid,
-	acct.fromperiod, acct.toperiod, acct.duedate, acct.disconnectiondate,
+	wbc.fromperiod, wbc.toperiod, wbc.duedate, wbc.disconnectiondate,
 	acct.stuboutid, acct.stuboutindex AS sortorder, acct.currentreading as lastreading
 FROM waterworks_account acct 
+	INNER JOIN waterworks_billing_cycle wbc on acct.billingcycleid=wbc.objid  
 	INNER JOIN waterworks_meter wm ON acct.meterid = wm.objid
 	INNER JOIN waterworks_stubout ws ON acct.stuboutid = ws.objid
 	INNER JOIN waterworks_sector_zone wsz ON ws.zoneid = wsz.objid 
@@ -13,9 +14,9 @@ FROM waterworks_account acct
 	INNER JOIN waterworks_sector s ON wsr.sectorid = s.objid
 WHERE s.objid = $P{sectorid} 
 	AND wsr.assignee_objid = $P{assigneeid}  
-	AND CURDATE() >= acct.readingdate 
+	AND CURDATE() >= wbc.readingdate 
 	AND acct.objid NOT IN (SELECT objid FROM waterworks_mobile_info WHERE objid=acct.objid) 
-
+	
 [cancelDownload]
 DELETE FROM waterworks_mobile_info
 WHERE batchid = $P{batchid}

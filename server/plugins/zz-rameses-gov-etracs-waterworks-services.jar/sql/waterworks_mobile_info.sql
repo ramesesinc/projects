9feb218ff0e44330e821184ff68d734a33d09617
worgflores <worgflores@gmail.com>
2016-05-05 09:33:55 +0800

    PLUGINS: WATERWORKS: updated waterworks_mobile_info sql

--- server/plugins/zz-rameses-gov-etracs-waterworks-services.jar/sql/waterworks_mobile_info.sql
+++ server/plugins/zz-rameses-gov-etracs-waterworks-services.jar/sql/waterworks_mobile_info.sql
@@ -4,23 +4,24 @@ SELECT
 	wm.serialno, wm.sizeid AS metersize, s.objid AS sectorid, s.code AS sectorcode, 
 	acct.classificationid, acct.address_barangay_objid AS barangayid,
 	wbc.fromperiod, wbc.toperiod, wbc.duedate, wbc.disconnectiondate,
-	acct.stuboutid, acct.stuboutindex AS sortorder, acct.currentreading as lastreading
+	acct.stuboutid, wsn.indexno AS sortorder, acct.currentreading as lastreading
 FROM waterworks_account acct 
 	INNER JOIN waterworks_billing_cycle wbc on acct.billingcycleid=wbc.objid  
 	INNER JOIN waterworks_meter wm ON acct.meterid = wm.objid
-	INNER JOIN waterworks_stubout ws ON acct.stuboutid = ws.objid
+	INNER JOIN waterworks_stubout ws ON acct.stuboutid = ws.objid 
+	INNER JOIN waterworks_stubout_node wsn on acct.stuboutnodeid=wsn.objid 
 	INNER JOIN waterworks_sector_zone wsz ON ws.zoneid = wsz.objid 
 	INNER JOIN waterworks_sector_reader wsr ON wsz.readerid = wsr.objid
 	INNER JOIN waterworks_sector s ON wsr.sectorid = s.objid
 WHERE s.objid = $P{sectorid} 
-	AND wsr.assignee_objid = $P{assigneeid}  
+	AND wsr.assignee_objid = $P{assigneeid} 
 	AND CURDATE() >= wbc.readingdate 
 	AND acct.objid NOT IN (SELECT objid FROM waterworks_mobile_info WHERE objid=acct.objid) 
-	
+
 [cancelDownload]
-DELETE FROM waterworks_mobile_info
+DELETE FROM waterworks_mobile_info 
 WHERE batchid = $P{batchid}
-AND objid NOT IN ${downloadedlist}
+	AND objid NOT IN ${downloadedlist}
 
 [findConsumptionAccount]
 SELECT ia.* 
@@ -30,25 +31,22 @@ WHERE ct.handler='waterworks' and cta.tag='WFEE'
 	AND cta.account_objid=ia.objid 
 
 [getSectorByUser]
-SELECT
-s.*
+SELECT s.*
 FROM waterworks_sector s
 	INNER JOIN waterworks_sector_reader sr ON s.objid = sr.sectorid 
 WHERE sr.assignee_objid = $P{userid}
 
 [getStuboutsBySector]
-SELECT
-s.*
+SELECT s.*
 FROM waterworks_stubout s 
 	INNER JOIN waterworks_sector_zone sz ON s.zoneid = sz.objid
 	INNER JOIN waterworks_sector_reader sr ON sz.readerid = sr.objid 
 WHERE sz.sectorid = $P{sectorid}
-AND sr.assignee_objid = $P{userid}
+	AND sr.assignee_objid = $P{userid} 
 
 [getZoneBySector]
-SELECT
-z.*
+SELECT z.*
 FROM waterworks_sector_zone z 
 	INNER JOIN waterworks_sector_reader r ON z.readerid = r.objid 
 WHERE z.sectorid = $P{sectorid}
-AND r.assignee_objid = $P{userid}
+	AND r.assignee_objid = $P{userid} 

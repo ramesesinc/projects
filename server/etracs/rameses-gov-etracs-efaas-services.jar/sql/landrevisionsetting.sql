7ff96c48949dee63f3f7ee0da933101eac6540e0
dinoquimson <dinoquimson1992@gmail.com>
2016-05-12 09:31:15 +0800

    added rameses-gov-etracs-efaas-services.jar

---
+++ server/etracs/rameses-gov-etracs-efaas-services.jar/sql/landrevisionsetting.sql
@@ -0,0 +1,20 @@
+[getLandRySetting]
+SELECT * FROM landrysetting
+
+[getLandAssessLevel]
+SELECT * FROM landassesslevel
+
+[getLandAssessLevelRange]
+SELECT * FROM landassesslevelrange
+
+[getLcuvSpecificClass]
+SELECT * FROM lcuvspecificclass
+
+[getLcuvSubClass]
+SELECT * FROM lcuvsubclass
+
+[getLcuvStripping]
+SELECT * FROM lcuvstripping
+
+[getLandAdjustmentType]
+SELECT * FROM landadjustmenttype

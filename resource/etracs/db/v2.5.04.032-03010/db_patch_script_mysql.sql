ff9b8fedbc368a20cdd1da06ca591205c161e00f
worgflores <worgflores@gmail.com>
2016-06-03 14:56:54 +0800

    resource: updated patch script

--- resource/etracs/db/v2.5.04.032-03010/db_patch_script_mysql.sql
+++ resource/etracs/db/v2.5.04.032-03010/db_patch_script_mysql.sql
@@ -1,4 +1,24 @@
 
+CREATE TABLE `entity_relation` (
+  `objid` varchar(50) NOT NULL,
+  `entity_objid` varchar(50) DEFAULT NULL,
+  `relateto_objid` varchar(50) DEFAULT NULL,
+  `relation` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  UNIQUE KEY `uix_sender_receiver` (`entity_objid`,`relateto_objid`),
+  KEY `ix_sender_objid` (`entity_objid`) ,
+  KEY `ix_receiver_objid` (`relateto_objid`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+alter table entity_relation 
+	add CONSTRAINT `fk_entityrelation_entity_objid` 
+	FOREIGN KEY (`entity_objid`) REFERENCES `entity` (`objid`); 
+alter table entity_relation 
+	add CONSTRAINT `fk_entityrelation_relateto_objid` 
+	FOREIGN KEY (`relateto_objid`) REFERENCES `entity` (`objid`);
+
+
 drop view if exists vw_entityindividual
 ;
 create view vw_entityindividual 
@@ -20,4 +40,27 @@ select
 	e.mobileno, e.phoneno 
 from entity e 
 	inner join entityindividual ei on ei.objid=e.objid 
+;
+
+drop view if exists vw_entityrelation
+;
+create view vw_entityrelation 
+as 
+select er.*, e.entityno, e.name, e.address_text as addresstext, e.type  
+from entity_relation er 
+	inner join entity e on e.objid=er.relateto_objid 
+order by e.name 
+;
+
+drop view if exists vw_entityrelation_lookup
+;
+create view vw_entityrelation_lookup 
+as
+select er.*, 
+	e.entityno, e.name, e.address_text as addresstext, e.type, 
+	ei.lastname, ei.firstname, ei.middlename, ei.gender, ei.birthdate, 
+	e.mobileno, e.phoneno 
+from entity_relation er  
+  inner join entityindividual ei on ei.objid=er.relateto_objid 
+	inner join entity e on e.objid=ei.objid 
 ;

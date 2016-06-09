c7a4599ad231287e9519372887d5eebdc2bc0469
Elmo Nazareno <elmonazareno@gmail.com>
2016-06-09 15:18:21 +0800

    waterworks disconnection

--- server/plugins/rameses-gov-etracs-waterworks-services.jar/waterworks/facts/WaterConsumption.groovy
+++ server/plugins/rameses-gov-etracs-waterworks-services.jar/waterworks/facts/WaterConsumption.groovy
@@ -6,23 +6,29 @@ import com.rameses.util.*;
 
 public class WaterConsumption {
 
-	int month;
-	int year;
-    int volume;
-    double amount;
-    double amtdue;
+	int month = 0;
+	int year = 0;
+    int volume = 0;
+    double amount = 0.0;
+    double amtdue = 0.0;
     String status;
     Date duedate;
     String refid;
+    Date disconnectiondate;
+
+	public WaterConsumption() {
+
+	}
 
     public WaterConsumption(def o) {
-		month = o.month;
-		year = o.year;
-	    volume = o.volume;
-	    amount = o.amount;
-	    status = o.status;
-	    duedate = o.duedate;
-	    amtdue = o.amtdue; 
-	    refid = o.refid;
+		if(o.month) month = o.month;
+		if(o.year)  year = o.year;
+	    if(o.volume)  volume = o.volume;
+	    if(o.amount) amount = o.amount;
+	    if(o.status) status = o.status;
+	    if(o.duedate) duedate = o.duedate;
+	    if(o.amtdue) amtdue = o.amtdue; 
+	    if(o.refid) refid = o.refid;
+	    if(o.disconnectiondate) disconnectiondate = o.disconnectiondate;
     }
 }

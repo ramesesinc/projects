d3086bdadf5dcb781ec8f30825574b0613542829
dinoquimson <dinoquimson1992@gmail.com>
2016-06-30 16:19:58 +0800

    updates of efaas

---
+++ android/efaas/src/com/rameses/android/db/BldgAssessLevelDB.java
@@ -0,0 +1,13 @@
+package com.rameses.android.db;
+
+import com.rameses.android.AbstractDBMapper;
+import com.rameses.db.android.DBContext;
+
+public class BldgAssessLevelDB extends AbstractDBMapper {
+
+	@Override
+	public String getTableName() {
+		return "bldgassesslevel";
+	}	
+	
+}

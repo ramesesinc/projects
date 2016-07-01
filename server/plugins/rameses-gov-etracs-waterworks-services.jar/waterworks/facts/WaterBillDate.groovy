67806c44b6d13b5f49cb929777125d09cd770828
Elmo Nazareno <elmonazareno@gmail.com>
2016-07-01 09:31:47 +0800

    added payment order module

--- server/plugins/rameses-gov-etracs-waterworks-services.jar/waterworks/facts/WaterBillDate.groovy
+++ server/plugins/rameses-gov-etracs-waterworks-services.jar/waterworks/facts/WaterBillDate.groovy
@@ -14,4 +14,13 @@ public class WaterBillDate {
 	Date duedate;
 	Date disconnectiondate;
 
+	public WaterBillDate( def m ) {
+		if(m.year) year = m.year;
+		if(m.month) month = m.month;
+		if(m.fromperiod) fromperiod = m.fromperiod;
+		if(m.toperiod) toperiod = m.toperiod;
+		if(m.duedate) duedate =  m.duedate;
+		if(m.disconnectiondate) disconnectiondate = m.disconnectiondate;
+	}	
+
 }

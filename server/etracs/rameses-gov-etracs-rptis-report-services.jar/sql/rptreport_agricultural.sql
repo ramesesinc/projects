0e7c0e849bc337ca5cef68f5573b59e68f5eda13
Jessie S. Zamora <jzamss@gmail.com>
2016-07-15 14:49:22 +0800

    RPTIS: report: add agricultural land area by actual use summary

---
+++ server/etracs/rameses-gov-etracs-rptis-report-services.jar/sql/rptreport_agricultural.sql
@@ -0,0 +1,18 @@
+[getActualUseSummaryByArea]
+select 
+	b.name as barangay,
+	spc.name as specificclass, 
+	sum(ld.areaha) as area
+from faas f 
+	inner join rpu r on f.rpuid = r.objid 
+	inner join landdetail ld on r.objid = ld.landrpuid 
+	inner join lcuvspecificclass spc on ld.specificclass_objid = spc.objid 
+	inner join propertyclassification pc on spc.classification_objid = pc.objid 
+	inner join realproperty rp on f.realpropertyid = rp.objid 
+	inner join barangay b on rp.barangayid = b.objid 
+where f.lguid = $P{lguid}
+  and f.state = 'current' 
+  and pc.name = 'AGRICULTURAL' 
+  and r.taxable = $P{taxable}
+group by b.name, spc.name 
+order by b.name, spc.name 

53cebb2992a9c15294bf4c1a50110c6e9b189614
Jessie S. Zamora <jzamss@gmail.com>
2016-07-23 09:18:59 +0800

    LANDTAX: certification: add year and qtr info

--- resource/etracs/db/v2.5.04.032-03010/rpt-v2504-032-03010 update script - mssql.sql
+++ resource/etracs/db/v2.5.04.032-03010/rpt-v2504-032-03010 update script - mssql.sql
@@ -1,1 +1,12 @@
 /* v2.5.04.032-03010 */
+
+/*===========================================
+* CERTIFICATION UPDATE
+*===========================================*/
+
+alter table rptcertification add [year] int
+go 
+alter table rptcertification add [qtr] int
+go 
+
+  
--- resource/etracs/db/v2.5.04.032-03010/rpt-v2504-032-03010 update script - mysql.sql
+++ resource/etracs/db/v2.5.04.032-03010/rpt-v2504-032-03010 update script - mysql.sql
@@ -1,2 +1,11 @@
 /* v2.5.04.032-03010 */
 
+/*===========================================
+* CERTIFICATION UPDATE
+*===========================================*/
+
+alter table rptcertification 
+  add `year` int,
+  add qtr int ;
+
+  

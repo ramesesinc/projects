f2b2db696cbb51c94bd906be5683d7c20e93f2d9
Jessie S. Zamora <jzamss@gmail.com>
2016-09-12 10:01:57 +0800

    LANDTAX: cashreceipt: remove unused FK

--- resource/etracs/db/v2.5.04.032-03011/rpt-v2504-032-03011 update script - mysql.sql
+++ resource/etracs/db/v2.5.04.032-03011/rpt-v2504-032-03011 update script - mysql.sql
@@ -1,1 +1,3 @@
 /* v2.5.04.032-03011 */
+alter table cashreceiptitem_rpt_online drop foreign key FK_cashreceiptitem_rpt_online_rptledgeritem;
+alter table cashreceiptitem_rpt_online drop foreign key FK_cashreceiptitem_rpt_online_rptledgeritemqtrly;

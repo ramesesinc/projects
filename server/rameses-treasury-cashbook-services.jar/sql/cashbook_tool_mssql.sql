8611b68ac2c65622f58388697c030ac8710f215d
worgflores <worgflores@gmail.com>
2016-10-02 23:34:04 +0800

    SYSTEM: updated TREASURY: updated PLUGINS: CTC: updated

---
+++ server/rameses-treasury-cashbook-services.jar/sql/cashbook_tool_mssql.sql
@@ -0,0 +1,43 @@
+[removeCashBookEntry]
+delete from cashbook_entry 
+where objid=$P{objid} 
+
+[getEntriesByRef]
+select * from cashbook_entry 
+where refid=$P{refid} 
+
+[findRunningBalance]
+select sum(e.dr - e.cr) as balance 
+from cashbook_entry e 
+where e.parentid=$P{cashbookid} 
+	and e.[lineno] >= $P{startlineno} 
+	and e.[lineno] < $P{endlineno} 
+
+[getCashBookEntries]
+select * from cashbook_entry e 
+where e.parentid=$P{cashbookid} 
+	and e.[lineno] > $P{lineno}   
+order by e.[lineno] 
+
+[updateCashBookEntryBalance]
+update ce set 
+	ce.runbalance = $P{runbalance}, 
+	ce.[lineno] = $P{lineno}  
+from cashbook_entry ce 
+where ce.objid=$P{objid}
+
+[updateCashBookBalance]
+update a set 
+	a.totaldr = b.dr,
+	a.totalcr = b.cr,
+	a.endbalance = b.balance, 
+	a.currentlineno = b.maxlineno 
+from cashbook a, ( 
+	select 
+		sum(dr) as dr, sum(cr) as cr, 
+		sum(dr-cr) as balance, 
+		max([lineno]) as maxlineno 
+	from cashbook_entry 
+	where parentid=$P{cashbookid} 
+)b  
+where a.objid=$P{cashbookid} 

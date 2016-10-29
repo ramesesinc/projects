e848381e6c15355cdf19095edc703d54daf33014
Jessie S. Zamora <jzamss@gmail.com>
2016-10-29 15:55:00 +0800

    RPTIS: report: add list of other claimants

--- resource/etracs/db/v2.5.04.032-03012/rpt-v2504-032-03012 update script - mssql.sql
+++ resource/etracs/db/v2.5.04.032-03012/rpt-v2504-032-03012 update script - mssql.sql
@@ -1,3 +1,9 @@
 /* v2.5.04.032-03012 */
-INSERT INTO sys_usergroup (objid, title, domain, userclass, orgclass, role) VALUES ('RPT.MANAGEMENT', 'MANAGEMENT', 'RPT', NULL, NULL, 'MANAGEMENT');
-INSERT INTO sys_usergroup_permission (objid, usergroup_objid, object, permission, title) VALUES ('PER-41ea09c:157ef7851c2:-7df4', 'RPT.MANAGEMENT', 'report', 'txn-monitoring', 'Transaction Monitoring');
+INSERT INTO sys_usergroup (objid, title, domain, userclass, orgclass, role) VALUES ('RPT.MANAGEMENT', 'MANAGEMENT', 'RPT', NULL, NULL, 'MANAGEMENT')
+go 
+INSERT INTO sys_usergroup_permission (objid, usergroup_objid, object, permission, title) VALUES ('PER-41ea09c:157ef7851c2:-7df4', 'RPT.MANAGEMENT', 'report', 'txn-monitoring', 'Transaction Monitoring')
+go 
+
+create index ix_realproperty_claimno on realproperty(claimno)
+go 
+
--- resource/etracs/db/v2.5.04.032-03012/rpt-v2504-032-03012 update script - mysql.sql
+++ resource/etracs/db/v2.5.04.032-03012/rpt-v2504-032-03012 update script - mysql.sql
@@ -2,4 +2,4 @@
 INSERT INTO `sys_usergroup` (`objid`, `title`, `domain`, `userclass`, `orgclass`, `role`) VALUES ('RPT.MANAGEMENT', 'MANAGEMENT', 'RPT', NULL, NULL, 'MANAGEMENT');
 INSERT INTO `sys_usergroup_permission` (`objid`, `usergroup_objid`, `object`, `permission`, `title`) VALUES ('PER-41ea09c:157ef7851c2:-7df4', 'RPT.MANAGEMENT', 'report', 'txn-monitoring', 'Transaction Monitoring');
 
-
+create index ix_realproperty_claimno on realproperty(claimno);

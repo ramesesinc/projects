54955a93b3364901e1e34203c757690a2fb41a2d
Jessie S. Zamora <jzamss@gmail.com>
2016-11-09 14:17:51 +0800

    LANDTAX: report: add consolidated report on real property tax

---
+++ server/etracs/rameses-gov-etracs-rptis-landtax-report-services.jar/sql/landtaxreport_consolidated.sql
@@ -0,0 +1,23 @@
+[getClassifications]
+select * from propertyclassification order by orderno 
+
+[getList]
+select
+    rrd.dtgenerated,
+    b.name as barangay,
+    count(*) as rpucount,
+    sum(rrd.basic) as basic, 
+    sum(rrd.basicdisc) as basicdisc,
+    sum(rrd.basicint) as basicint,
+    sum(rrd.sef) as sef,
+    sum(rrd.sefdisc) as sefdisc,
+    sum(rrd.sefint) as sefint,
+    sum(rrd.basic - rrd.basicdisc + rrd.basicint + rrd.sef - rrd.sefdisc + rrd.sefint) as total 
+from report_rptdelinquency rrd 
+    inner join rptledger rl on rrd.rptledgerid = rl.objid 
+    inner join barangay b on rl.barangayid = b.objid 
+where rl.classification_objid like $P{classid}
+and rl.rputype like $P{type}
+and rl.taxable = 1 
+group by rrd.dtgenerated, b.name 
+order by b.pin 

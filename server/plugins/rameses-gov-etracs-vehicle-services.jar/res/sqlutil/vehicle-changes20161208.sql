e6ec63bd1f0c19c9b9dd33c130f13224fdc0bb2b
Elmo Nazareno <elmonazareno@gmail.com>
2016-12-08 12:39:25 +0800

    changes

---
+++ server/plugins/rameses-gov-etracs-vehicle-services.jar/res/sqlutil/vehicle-changes20161208.sql
@@ -0,0 +1,5 @@
+vehicle_application ( txnmode VARCHAR(50), billexpirydate DATE`vehicle_tricycle` )
+
+vehicle_tricycle ( chassisno VARCHAR(50),  sidecarcolor VARCHAR(50) )
+
+vehicle_franchise ( cluster VARCHAR(50), dtregistered DATE )

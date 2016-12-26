5fe504aee4da0045404e4c7b2e0864beadc790a7
worgflores <worgflores@gmail.com>
2016-12-26 18:33:01 +0800

    TREASURY: updated TCReportService

--- server/etracs/rameses-gov-treasury-services.jar/sql/tcreport.sql
+++ server/etracs/rameses-gov-treasury-services.jar/sql/tcreport.sql
@@ -1,17 +1,60 @@
 [getCollectionByFund]
 select 
-    ri.fund_title as fundname, cri.item_objid as acctid, cri.item_title as acctname,
-    cri.item_code as acctcode, sum( cri.amount ) as amount 
-from cashreceipt cr 
-  INNER JOIN remittance_cashreceipt rc on cr.objid = rc.objid 
-  INNER JOIN remittance r on r.objid = rc.remittanceid 
-  INNER join cashreceiptitem cri on cri.receiptid = cr.objid
-  INNER join itemaccount ri on ri.objid = cri.item_objid 
-  LEFT JOIN cashreceipt_void vr ON cr.objid = vr.receiptid  
-where r.remittancedate BETWEEN $P{fromdate} AND $P{todate} 
-  and vr.objid is null  ${filter} 
-group by ri.fund_title, cri.item_objid, cri.item_code, cri.item_title 
-order by fundname, acctcode, acctname  
+  ri.fund_objid as fundid, ri.fund_title as fundname, fund.code as fundcode, 
+  cri.item_objid as acctid, cri.item_title as acctname, cri.item_code as acctcode, 
+  sum( cri.amount ) as amount, fund.parentid as fundparentid  
+from ( 
+  select remc.objid 
+  from remittance r 
+    inner join remittance_cashreceipt remc on r.objid=remc.remittanceid  
+  where r.remittancedate >= $P{fromdate} 
+    and r.remittancedate < $P{todate} 
+    and remc.objid not in (select receiptid from cashreceipt_void where receiptid=remc.objid) 
+)xx1 
+  inner join cashreceipt cr on cr.objid=xx1.objid 
+  inner join cashreceiptitem cri on cri.receiptid=cr.objid 
+  inner join itemaccount ri on cri.item_objid=ri.objid 
+  inner join fund on ri.fund_objid=fund.objid 
+where ri.fund_objid in ( 
+    select objid from fund where objid like $P{fundid} 
+    union 
+    select objid from fund where parentid=$P{fundparentid} 
+  ) 
+group by 
+  ri.fund_objid, ri.fund_title, fund.code, fund.parentid, 
+  cri.item_objid, cri.item_title, cri.item_code 
+order by 
+  fund.code, ri.fund_title, cri.item_code, cri.item_title  
+
+
+[getCollectionByFundByLiquidation]
+select 
+  fund.parentid as fundparentid, fund.objid as fundid, 
+  fund.code as fundcode, fund.title as fundname, 
+  ia.objid as acctid, ia.code as acctcode, ia.title as acctname, 
+  xx2.amount 
+from ( 
+  select 
+    inc.fundid, inc.acctid, sum(inc.amount) as amount 
+  from ( 
+    select lrem.objid as refid 
+    from liquidation l 
+      inner join liquidation_remittance lrem on l.objid=lrem.liquidationid 
+    where l.dtposted >= $P{fromdate} 
+      and l.dtposted < $P{todate} 
+  )xx1, income_summary inc 
+  where inc.refid = xx1.refid  
+  group by inc.fundid, inc.acctid 
+)xx2 
+  inner join fund on xx2.fundid=fund.objid 
+  inner join itemaccount ia on xx2.acctid=ia.objid 
+where fund.objid in ( 
+    select objid from fund where objid like $P{fundid} 
+    union 
+    select objid from fund where parentid = $P{fundparentid} 
+  ) 
+order by 
+  fund.code, fund.title, ia.code, ia.title 
 
 
 [getAbstractOfCollection]
@@ -38,10 +81,8 @@ where r.remittancedate BETWEEN $P{fromdate} AND $P{todate}
 order by cr.formno, cr.receiptno
 
 
-  
-
 [getFunds]
-select * from fund 
+select * from fund order by code 
 
 [getSubFunds]
-select * from fund where parentid = $P{objid}
+select * from fund where parentid = $P{objid} order by code 

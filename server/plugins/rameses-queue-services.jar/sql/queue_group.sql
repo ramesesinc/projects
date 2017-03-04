e1bd4a18e28ed81716984b1c43a6d28b83946e3a
elmonazareno <elmonazareno@gmail.com>
2017-03-04 14:32:42 +0800

    WEB: KIOSK: added kiosk webapp

--- server/plugins/rameses-queue-services.jar/sql/queue_group.sql
+++ server/plugins/rameses-queue-services.jar/sql/queue_group.sql
@@ -5,7 +5,12 @@ select
 		select n.seriesno from queue_number_counter nc 
 			inner join queue_number n on nc.objid=n.objid 
 		where nc.counterid=c.objid and n.sectionid=s.objid  
-	) as seriesno  	
+	) as seriesno, 
+	( 
+		SELECT n.ticketno FROM queue_number_counter nc 
+			INNER JOIN queue_number n ON nc.objid=n.objid 
+		WHERE nc.counterid=c.objid AND n.sectionid=s.objid  
+	) AS ticketno   	
 from queue_section s 
 	inner join queue_counter_section cs on s.objid=cs.sectionid 
 	inner join queue_counter c on cs.counterid=c.objid 

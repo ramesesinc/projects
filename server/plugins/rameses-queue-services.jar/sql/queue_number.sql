f03ded9dc3ae4e494bbc87116b0a07c21953c005
elmonazareno <elmonazareno@gmail.com>
2017-03-10 16:50:25 +0800

    TREASURY: EOR: updated
    PLUGINS: QUEUE: updated

--- server/plugins/rameses-queue-services.jar/sql/queue_number.sql
+++ server/plugins/rameses-queue-services.jar/sql/queue_number.sql
@@ -5,7 +5,7 @@ where sectionid=$P{sectionid}
 	and not exists ( 
 		select objid from queue_number_counter where objid=qn.objid 
 	) 
-order by seriesno 
+order by txndate, seriesno 
 
 [getPendingList]
 select * from ( 
@@ -34,3 +34,38 @@ where ( txndate IS NULL or txndate < $P{txndate} )
 		where objid=queue_number.objid 
 	) 
 
+
+[getSkipTickets]
+select * from ( 
+	select qn.*, qs.title as sectiontitle  
+	from queue_counter_section qcs 
+		inner join queue_section qs on qcs.sectionid=qs.objid 
+		inner join queue_number qn on qn.sectionid=qs.objid 
+	where qcs.counterid=$P{counterid} 
+		and qn.state='SKIP' 
+		and qn.txndate >= $P{startdate} 
+		and qn.txndate < $P{enddate} 
+)xxx 
+order by xxx.txndate desc, xxx.ticketno desc 
+
+
+[getStatusList]
+select qs.*, ( 
+		select count(*) from queue_number qn 
+		where qn.sectionid=qs.objid 
+			and qn.state='PENDING' 
+			and qn.objid not in (select objid from queue_number_counter where objid=qn.objid) 
+			and qn.txndate >= $P{startdate} 
+			and qn.txndate < $P{enddate}  
+	) as pendingcount, 
+	( 
+		select count(*) from queue_number qn 
+		where qn.sectionid=qs.objid 
+			and qn.state='SKIP' 
+			and qn.txndate >= $P{startdate} 
+			and qn.txndate < $P{enddate} 			
+	) as skipcount 
+from queue_counter_section qcs 
+	inner join queue_section qs on qcs.sectionid=qs.objid 
+where qcs.counterid=$P{counterid} 
+order by qs.groupid, qs.title 

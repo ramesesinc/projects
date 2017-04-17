6c459b346b954f3f1ad6f9483be1e41395f2cd04
Jessie S. Zamora <jzamss@gmail.com>
2017-04-17 17:56:38 +0800

    RPTIS: report: titled faas listing: add taxpayer filter option

--- server/etracs/rameses-gov-etracs-rptis-report-services.jar/sql/rptreport_titledfaaslisting.sql
+++ server/etracs/rameses-gov-etracs-rptis-report-services.jar/sql/rptreport_titledfaaslisting.sql
@@ -1,5 +1,6 @@
 [getTitledProperties]
 select 
+	b.name as barangay,
 	f.state,
 	f.tdno,
 	r.fullpin,
@@ -19,12 +20,14 @@ select
 from faas f 
 	inner join rpu r on f.rpuid = r.objid
 	inner join realproperty rp on f.realpropertyid = rp.objid 
+	inner join barangay b on rp.barangayid = b.objid 
 	inner join propertyclassification pc on r.classification_objid = pc.objid 
 where f.lguid like $P{lguid}
    and rp.barangayid like $P{barangayid}
    and rp.section like $P{section}
+   and f.taxpayer_objid like $P{taxpayerid}
    and f.state = 'CURRENT'
    and r.rputype = 'land'
    and f.titleno is not null 
-order by f.tdno  	
+order by b.name, f.tdno  	
 

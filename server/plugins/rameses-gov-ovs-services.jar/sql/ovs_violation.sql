3451ef01c256aae745f66982b3d2a7229fcca066
worgflores <worgflores@gmail.com>
2017-04-20 12:43:11 +0800

    PLUGINS: OVS: updated

--- server/plugins/rameses-gov-ovs-services.jar/sql/ovs_violation.sql
+++ server/plugins/rameses-gov-ovs-services.jar/sql/ovs_violation.sql
@@ -1,8 +1,8 @@
 [getViolationsByViolator]
 SELECT
 v.*
-FROM violation v 
-INNER JOIN violation_ticket_entry vte ON v.objid = vte.violationid 
-INNER JOIN violation_ticket vt ON vte.parentid = vt.objid 
+FROM ovs_violation v 
+INNER JOIN ovs_violation_ticket_entry vte ON v.objid = vte.violationid 
+INNER JOIN ovs_violation_ticket vt ON vte.parentid = vt.objid 
 WHERE v.objid = $P{violationid} 
 AND vt.violator_objid = $P{violatorid}
--- server/plugins/rameses-gov-ovs-services.jar/sql/ovs_violation_ticket.sql
+++ server/plugins/rameses-gov-ovs-services.jar/sql/ovs_violation_ticket.sql
@@ -1,33 +1,33 @@
 [getViolations]
 SELECT
 v2.*
-FROM violation_ticket v1
-INNER JOIN violation v2 on v1.violationid = v2.objid
+FROM ovs_violation_ticket v1
+INNER JOIN ovs_violation v2 on v1.violationid = v2.objid
 WHERE parentid = $P{objid}
 
 [getViolationsPerTicket]
 SELECT
 v.*,
 vte.*
-FROM violation_ticket_entry vte 
-INNER JOIN violation v ON vte.violationid = v.objid
+FROM ovs_violation_ticket_entry vte 
+INNER JOIN ovs_violation v ON vte.violationid = v.objid
 WHERE vte.parentid = $P{objid}
 
 [findVehiclePerTicket]
 SELECT
 v.*,
 vtv.*
-FROM violation_ticket_vehicle vtv
-INNER JOIN vehicle v ON vtv.vehicleid = v.objid 
+FROM ovs_violation_ticket_vehicle vtv
+INNER JOIN ovs_vehicle v ON vtv.vehicleid = v.objid 
 WHERE vtv.parentid = $P{objid}
 
 [getViolationTicketsByPaymentRefid]
 select distinct 
 	vt.objid, vt.ticketno, 
 	vt.apprehensionofficer_name as officername  
-from payment op 
-	inner join payment_item opi on opi.parentid=op.objid 
-	inner join violation_ticket_entry vte on vte.objid=opi.refid 
-	inner join violation_ticket vt on vt.objid=vte.parentid 
+from ovs_payment op 
+	inner join ovs_payment_item opi on opi.parentid=op.objid 
+	inner join ovs_violation_ticket_entry vte on vte.objid=opi.refid 
+	inner join ovs_violation_ticket vt on vt.objid=vte.parentid 
 where op.refid=$P{refid} 
 order by vt.ticketno 

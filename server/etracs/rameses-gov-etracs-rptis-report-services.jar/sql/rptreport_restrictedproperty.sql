c4cbe66d39d1d10acadee6b59428ce776b5f3810
Jessie S. Zamora <jzamss@gmail.com>
2017-07-07 11:39:52 +0800

    RPTIS: faas: stewardship update

--- server/etracs/rameses-gov-etracs-rptis-report-services.jar/sql/rptreport_restrictedproperty.sql
+++ server/etracs/rameses-gov-etracs-rptis-report-services.jar/sql/rptreport_restrictedproperty.sql
@@ -1,48 +1,46 @@
 [getRestrictedProperties]
 select 
     b.name as barangay, 
-    rl.tdno, rl.fullpin, rl.owner_name, rl.administrator_name, 
-    rl.cadastrallotno, rl.classcode, 
-    rl.totalareaha, rl.totalareaha * 10000 as totalareasqm,
-    rl.totalmv, rl.totalav,
+    f.tdno, f.fullpin, f.owner_name, f.administrator_name, 
+    rp.cadastrallotno, pc.code as classcode, 
+    r.totalareaha, r.totalareasqm,
+    r.totalmv, r.totalav,
     frt.name as restrictiontype,
     fr.remarks
-from rptledger rl 
-    inner join faas f on rl.faasid = f.objid 
+from faas f 
     inner join realproperty rp on f.realpropertyid = rp.objid 
-    inner join barangay b on rl.barangayid = b.objid 
-    inner join faas_restriction fr on rl.objid = fr.ledger_objid 
+    inner join rpu r on f.rpuid = r.objid 
+    inner join propertyclassification pc on r.classification_objid = pc.objid 
+    inner join barangay b on rp.barangayid = b.objid 
+    inner join faas_restriction fr on f.objid  = fr.parent_objid  
     inner join faas_restriction_type frt on fr.restrictiontype_objid = frt.objid 
 where f.lguid like $P{lguid}
-and rl.barangayid like $P{barangayid}
+and rp.barangayid like $P{barangayid}
 and rp.section like $P{section}
 and fr.state = 'ACTIVE'
 and fr.txndate >= $P{startdate} and fr.txndate < $P{enddate}
-order by rl.tdno 
+order by f.tdno 
 
 
 [getUnrestrictedProperties]
 select 
     b.name as barangay, 
-    rl.tdno, rl.fullpin, rl.owner_name, rl.administrator_name, 
-    rl.cadastrallotno, rl.classcode, 
-    rl.totalareaha, rl.totalareaha * 10000 as totalareasqm,
-    rl.totalmv, rl.totalav,
+    f.tdno, f.fullpin, f.owner_name, f.administrator_name, 
+    rp.cadastrallotno, pc.code as classcode, 
+    r.totalareaha, r.totalareasqm,
+    r.totalmv, r.totalav,
     frt.name as restrictiontype,
-    fr.remarks,
-    fr.receipt_receiptno as receiptno,
-    fr.receipt_receiptdate as receiptdate, 
-    fr.receipt_lastyearpaid as lastyearpaid,
-    fr.receipt_lastqtrpaid as lastqtrpaid
-from rptledger rl 
-    inner join faas f on rl.faasid = f.objid 
+    fr.remarks
+from faas f 
     inner join realproperty rp on f.realpropertyid = rp.objid 
-    inner join barangay b on rl.barangayid = b.objid 
-    inner join faas_restriction fr on rl.objid = fr.ledger_objid 
+    inner join rpu r on f.rpuid = r.objid 
+    inner join propertyclassification pc on r.classification_objid = pc.objid 
+    inner join barangay b on rp.barangayid = b.objid 
+    inner join faas_restriction fr on f.objid  = fr.parent_objid  
     inner join faas_restriction_type frt on fr.restrictiontype_objid = frt.objid 
 where f.lguid like $P{lguid}
-and rl.barangayid like $P{barangayid}
+and rp.barangayid like $P{barangayid}
 and rp.section like $P{section}
 and fr.state = 'UNRESTRICTED'
 and fr.txndate >= $P{startdate} and fr.txndate < $P{enddate}
-order by rl.tdno 
+order by f.tdno 

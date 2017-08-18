b982a423a1fde4f381aa852e2901566edf30aa50
Jessie S. Zamora <jzamss@gmail.com>
2017-08-18 10:04:28 +0800

    EOR: realty tax collection and remote sync support

--- resource/etracs/db/v2.5.04.032-03018/rpt-v2504-032-03017 update script - mssql.sql
+++ resource/etracs/db/v2.5.04.032-03018/rpt-v2504-032-03017 update script - mssql.sql
@@ -2,3 +2,6 @@
 
 alter table faasbacktax alter column tdno varchar(25) null
 go 
+
+drop table cashreceiptpayment_eor
+go 
--- server/online/rameses-gov-etracs-online-services.jar/scripts/sql/onlinecashreceipt.sql
+++
@@ -1,8 +0,0 @@
-[getFundSummaries]
-select 
-	ia.fund_objid, ia.fund_code, ia.fund_title, ia.fund_title as fund_name,
-	sum(cri.amount) as amount 
-from cashreceiptitem cri 
-inner join itemaccount ia on cri.item_objid = ia.objid 
-where cri.receiptid = $P{objid}
-group by ia.fund_objid, ia.fund_code, ia.fund_title

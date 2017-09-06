f5457daec6c33654c005ce0a78a1564aed1acee5
worgflores <worgflores@gmail.com>
2017-09-06 13:14:32 +0800

    ACCOUNTING: updated
    TREASURY: updated

--- server/rameses-enterprise-treasury-rule-services.jar/treasury/actions/AddRevenueShareByOrg.groovy
+++ server/rameses-enterprise-treasury-rule-services.jar/treasury/actions/AddRevenueShareByOrg.groovy
@@ -6,28 +6,28 @@ import java.util.*;
 import treasury.facts.*;
 import com.rameses.osiris3.common.*;
 
-
-
-class AddRevenueShare implements RuleActionHandler  {
+class AddRevenueShareByOrg implements RuleActionHandler  {
 
 	public void execute(def params, def drools) {
 
 		def refacct = params.refaccount;
-		def payableacct = params.payableaccount;
+		def payableacct = params.payableparentaccount;
 		def share = params.share.intValue();
 		def amt = params.amount.decimalValue;
+		def org = params.org;
 
 		if( refacct ==null && payableacct==null)
-			throw new Exception("Error in AddRevenueShare action. Please indicate a ref account or a payable account. Check the rules");
+			throw new Exception("Error in AddRevenueShareByOrg action. Please indicate a ref account or a payable parent account. Check the rules");
 
 		def ct = RuleExecutionContext.getCurrentContext();
 		def rs = new RevenueShare();
 
-		if(refacct?.key!=null && refacct?.key!='null') {
+		if ( refacct?.key!=null && refacct?.key!='null' ) {
 			rs.refaccount = ct.env.acctUtil.createAccountFact( [objid: refacct.key] );
-		};	
-		if(payableacct?.key!=null && payableacct?.key!='null') {
-			rs.payableaccount = ct.env.acctUtil.createAccountFact( [objid: payableacct.key] );
+		} 
+		if ( payableacct?.key != null && payableacct?.key!='null' ) {
+			rs.payableaccount = ct.env.acctUtil.createAccountFactByOrg( payableacct.key, org.orgid ); 
+			if ( !rs.payableaccount ) throw new Exception('There is no payable account with parent '+ payableacct.value + ' org '+ org.orgid);
 		}
 		rs.amount  = amt;
 		rs.share = share;

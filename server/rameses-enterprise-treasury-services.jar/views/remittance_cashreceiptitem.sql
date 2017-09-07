6f943f029f11b98f0bdf5d4dfaf72df225729ddb
elmonazareno <elmonazareno@gmail.com>
2017-09-07 15:34:15 +0800

    added cashreceiptitem for additional checking

--- server/rameses-enterprise-treasury-services.jar/views/remittance_cashreceiptitem.sql
+++ server/rameses-enterprise-treasury-services.jar/views/remittance_cashreceiptitem.sql
@@ -10,11 +10,13 @@ SELECT
     ci.receiptid,
     r.objid AS remittanceid,
     rf.objid AS remittancefundid,
-    rf.`liquidationfundid`
+    rf.`liquidationfundid`,
+    lf.liquidationid 
 FROM cashreceiptitem ci
 INNER JOIN cashreceipt c ON ci.`receiptid`=c.objid 
 INNER JOIN remittance r ON c.`remittanceid`=r.objid 
 INNER JOIN remittance_fund rf ON rf.`remittanceid`=r.objid AND rf.`fund_objid`=ci.`item_fund_objid` 
 LEFT JOIN cashreceipt_void cv ON c.objid=cv.receiptid
+LEFT JOIN liquidation_fund lf ON lf.objid=rf.liquidationfundid 
 WHERE cv.objid  IS NULL
 

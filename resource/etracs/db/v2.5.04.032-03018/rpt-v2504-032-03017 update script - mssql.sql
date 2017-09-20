27bfdee4f25b0e601902124b57ac6a215902a3e3
Jessie S. Zamora <jzamss@gmail.com>
2017-09-20 10:12:05 +0800

    RPTIS: land: fix gr error on missing master data mapping (set nullable columns)

--- resource/etracs/db/v2.5.04.032-03018/rpt-v2504-032-03017 update script - mssql.sql
+++ resource/etracs/db/v2.5.04.032-03018/rpt-v2504-032-03017 update script - mssql.sql
@@ -2,3 +2,14 @@
 
 alter table faasbacktax alter column tdno varchar(25) null
 go 
+
+
+
+alter table landdetail alter column subclass_objid varchar(50) null
+go 
+alter table landdetail alter column specificclass_objid varchar(50) null
+go 
+alter table landdetail alter column actualuse_objid varchar(50) null
+go 
+alter table landdetail alter column landspecificclass_objid varchar(50) null
+go 
--- resource/etracs/db/v2.5.04.032-03018/rpt-v2504-032-03017 update script - mysql.sql
+++ resource/etracs/db/v2.5.04.032-03018/rpt-v2504-032-03017 update script - mysql.sql
@@ -1,3 +1,9 @@
 /* 254032-03018 */
 
 alter table faasbacktax modify column tdno varchar(25) null;
+
+
+alter table landdetail modify column subclass_objid varchar(50) null;
+alter table landdetail modify column specificclass_objid varchar(50) null;
+alter table landdetail modify column actualuse_objid varchar(50) null;
+alter table landdetail modify column landspecificclass_objid varchar(50) null;

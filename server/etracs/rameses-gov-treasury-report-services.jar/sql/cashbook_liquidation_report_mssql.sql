6a7d56a60d5770725c194dd1e789ead3ca36b6a3
worgflores <worgflores@gmail.com>
2017-11-13 13:32:53 +0800

    GOV: TREASURY: added treasury report project

---
+++ server/etracs/rameses-gov-treasury-report-services.jar/sql/cashbook_liquidation_report_mssql.sql
@@ -0,0 +1,30 @@
+[getReport]
+select * 
+from ( 
+	select 
+		convert(DATE, l.dtposted) as refdate, l.liquidatingofficer_name as username, 
+		l.txnno as refno, 'liquidation' as reftype, 0.0 as dr, lf.amount as cr, 
+		(lf.amount * -1.0) as amount, 1 as sortindex 
+	from liquidation l 
+		inner join liquidation_cashier_fund lf on lf.liquidationid = l.objid 
+	where l.liquidatingofficer_objid = $P{accountid}  
+		and l.dtposted >= $P{startdate}  
+		and l.dtposted < $P{enddate} 
+		and lf.fund_objid = $P{fundid}  
+	
+	union all 
+
+	select 
+		convert(DATE, l.dtposted) as refdate, rem.collector_name as username, 
+		rem.txnno as refno, 'remittance' as reftype, remf.amount as dr, 0.0 as cr, 
+		remf.amount as amount, 0 as sortindex 
+	from liquidation l 
+		inner join liquidation_remittance lrem on lrem.liquidationid = l.objid 
+		inner join remittance rem on rem.objid = lrem.objid 
+		inner join remittance_fund remf on remf.remittanceid = rem.objid 
+	where l.liquidatingofficer_objid = $P{accountid}  
+		and l.dtposted >= $P{startdate}  
+		and l.dtposted < $P{enddate} 
+		and remf.fund_objid = $P{fundid}  
+)tmp1 
+order by refdate, sortindex, username, refno  

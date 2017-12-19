f30617e81d98dce6eac36f9ddfb7973c847db4ba
elmonazareno <elmonazareno@gmail.com>
2017-12-19 15:59:44 +0800

    updated market and treasury

---
+++ client/etracs/rameses-gov-treasury/src/com/rameses/enterprise/treasury/models/AFControlModel.groovy
@@ -0,0 +1,62 @@
+package com.rameses.enterprise.treasury.models;
+
+import com.rameses.rcp.common.*;
+import com.rameses.rcp.annotations.*;
+import com.rameses.osiris2.client.*;
+import com.rameses.osiris2.common.*;
+import com.rameses.seti2.models.*;
+
+class AFControlModel extends CrudFormModel {
+
+    @Service("AFControlService")
+    def service;
+    
+    void activate() {
+        if(entity.active==1)
+            throw new Exception("Entry is already active");
+        service.activateControl( entity );
+        if(caller) caller.reloadEntity();
+    }
+
+    void deactivate() {
+        if(entity.active==0)
+            throw new Exception("Entry is already inactive");
+        service.deactivateControl( entity );
+        if(caller) caller.reloadEntity();
+    }
+    
+     def assignFund() {
+        if( entity.fund?.objid ) {
+            throw new Exception("Fund is already assigned");
+        }
+        def h = { o->
+            service.assignFund( [objid: entity.objid, fund: o ] );
+            if(caller) caller.reloadEntity();
+        };
+        return InvokerUtil.lookupOpener('fund:lookup', [onselect: h]);
+    }
+
+    def unassignFund() {
+        if( !entity.fund?.objid ) {
+            throw new Exception("Fund is already unassigned");
+        }
+        service.unassignFund( [objid: entity.objid ] );
+        if(caller) caller.reloadEntity();
+    }
+
+    def assignSubcollector() {
+        def h = { o->
+            service.assignSubcollector( [objid: entity.objid, assignee: o ] );
+            if(caller) caller.reloadEntity();
+        };
+        return InvokerUtil.lookupOpener('subcollector:lookup', [onselect: h]);
+    }
+
+    def unassignSubcollector() {
+        service.unassignSubcollector( [objid: entity.objid, owner: entity.owner ] );
+        if(caller) caller.reloadEntity();
+    }
+
+    
+    
+}    
---
+++ client/etracs/rameses-gov-treasury/src/com/rameses/enterprise/treasury/models/AFTxnModel.groovy
@@ -0,0 +1,185 @@
+package com.rameses.enterprise.treasury.models;
+
+import com.rameses.rcp.common.*;
+import com.rameses.rcp.annotations.*;
+import com.rameses.osiris2.client.*;
+import com.rameses.osiris2.common.*;
+import com.rameses.seti2.models.*;
+import com.rameses.util.*;
+
+class AFTxnModel extends CrudPageFlowModel {
+
+    @Service("AFTxnService")
+    def svc;
+    
+    @Service("DateService")
+    def dateSvc;
+    
+    boolean withrequest = false;
+    def afrequest;
+    
+    def txnTypes;
+    def reqtype = null;
+    def selectedItem;
+    
+    def itemTxnTypes;
+    
+    void create() {
+        entity = [:];
+        entity.state = 'DRAFT';
+        entity.items = [];
+        afrequest = null;
+        itemListHandler.reload();
+        entity.dtfiled = dateSvc.getBasicServerDate();
+    }
+    
+    void initNew() {
+        if(afrequest!=null) {
+            def req = persistenceService.read( [_schemaname:'afrequest', objid: afrequest.objid ] );
+            entity.request = [objid:req.objid , reqno: req.reqno];
+            entity.issueto = req.requester;
+            entity.respcenter = req.respcenter;
+            req.items.each {
+                def m = [:];
+                m.item = it.item;
+                m.unit = it.unit;
+                m.qtyrequested = it.qty;
+                m.qty = it.qty;
+                m.qtyserved = 0;
+                m.cost = it.afunit.saleprice;
+                m.txntype = req.reqtype;
+                m.afunit = it.afunit;
+                computeLineTotal(m);
+                entity.items << m;
+            }
+            itemTxnTypes = [ req.reqtype ];
+        }
+        else {
+            if( entity.txntype.matches('ISSUE|RETURN|TRANSFER') ) itemTxnTypes = ["COLLECTION", "SALE"];
+            else if(entity.txntype == 'PURCHASE_RECEIPT' ) itemTxnTypes = ['PURCHASE'];
+            else if(entity.txntype == 'BEGIN_BALANCE' ) itemTxnTypes = ['BEGIN'];
+            else if( entity.txntype == 'FORWARD' ) itemTxnTypes = ['FORWARD']; 
+            else throw new Exception("Unrecognized txntype " + entity.txntype );
+        }
+    }
+
+    //the only lookup here is for the request because the other request os for purchase (af receipt)
+    public def getLookupRequest() {
+        if( entity.txntype == "ISSUE" ) {
+            return Inv.lookupOpener( "afrequest_collection:lookup", [:] );
+        }
+        else {
+            return Inv.lookupOpener( "afrequest_purchase:lookup", [:] );
+        }
+    }
+    
+    def itemListHandler = [
+        fetchList : { o->
+            return entity.items;
+        },
+        onAddItem: { o-> 
+            //if ( !o.qtyrequested ) o.qtyrequested = o.qtyissued;
+            entity.items << o;
+        },
+        onRemoveItem: { o->
+            entity.items.remove(o);
+        },
+        onColumnUpdate: { o,colName->
+            if ( colName == "item" ) {
+                o.item.objid = o.item.itemid;
+                o.unit = o.item.unit; 
+                o.cost = o.item.saleprice; 
+                computeLineTotal( o ); 
+            }
+            else if(colName=="qty") {
+                if ( afrequest && o.qty > o.qtyrequested ) 
+                    throw new Exception("Qty must be less than qty requested");
+                computeLineTotal( o ); 
+            } 
+            else if(colName=="cost") {
+                computeLineTotal( o ); 
+            } 
+        }
+    ] as EditorListModel;
+    
+    private void computeLineTotal( o ) {
+        o.linetotal = (o.qty ? o.qty : 0) * (o.cost ? o.cost : 0.0);
+    }
+    
+    public def getInfo() {
+        return TemplateProvider.instance.getResult( "com/rameses/enterprise/treasury/views/AFTxnViewPage.gtpl", [entity:entity] );
+    }
+    
+    void saveCreate() {
+        def b = MsgBox.confirm( "You are about to save the draft document. Proceed?" );
+        if(!b) throw new BreakException();
+        super.saveCreate();
+    }
+    
+    void open() {
+        def e = [_schemaname:'aftxn', objid: entity.objid];
+        entity = persistenceService.read( e );
+    }
+    
+    def startOpen() {
+        String state = (entity.state == 'DRAFT') ? 'open' : 'posted';
+        return super.start( state );
+    }
+    
+    void post() {
+        svc.post([objid:entity.objid, txntype: entity.txntype ]);
+        reloadEntity();
+    }
+
+    void reloadEntity() {
+        open();
+        binding.refresh();
+    }
+    
+    //**************************************************************************
+    //PURCHASE RECEIPT 
+    //**************************************************************************/
+    def addBatchEntry( def o ) {
+        return Inv.lookupOpener( "aftxn_control_entry:add", [ 
+            entity:entity, params: o, handler:{ vv-> reloadEntity(); } 
+        ]);
+    }
+    
+    void removeBatchEntry(def o) {
+        if( !MsgBox.confirm('You are about to remove the entered accountable forms. Proceed?') ) return;
+        o.refid = entity.objid;
+        o.txntype = entity.txntype;
+        try {
+            svc.removeBatchEntry(o);
+            reloadEntity();
+        }
+        catch(e) {
+            MsgBox.err(e);
+        }
+    }
+    
+    //**************************************************************************
+    //AF ISSUANCE 
+    //**************************************************************************/
+    void assignAvailableStock() {
+        MsgBox.alert( "entity is " + entity.objid );
+    }
+    
+    def issueBatch( def o ) {
+        o.refid = entity.objid;
+        o.reftype = entity.txntype;
+        o.refdate = entity.dtfiled;
+        o.refno = entity.controlno;
+        try {
+            svc.issue(o);
+            reloadEntity();
+        }
+        catch(e) {
+            MsgBox.err(e);
+        }
+    }
+    
+    def editEntry( def o ) {
+        return Inv.lookupOpener("af_control_detail:lookup", ['query.refitemid': o.refitemid ]);
+    }
+}    

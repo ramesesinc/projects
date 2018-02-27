5c818137ce20f4cd312f099e310886b6a9b40b40
elmonazareno <elmonazareno@gmail.com>
2018-02-27 16:14:22 +0800

    pls. add jevitem view

--- client/rameses-enterprise-accounting/src/com/rameses/enterprise/accounting/models/JevModel.groovy
+++ client/rameses-enterprise-accounting/src/com/rameses/enterprise/accounting/models/JevModel.groovy
@@ -9,20 +9,53 @@ import com.rameses.seti2.models.*;
 class JevModel  extends CrudFormModel { 
 
    String printFormName = 'jev';
+   
+   def fixSubList( def list ) {
+       def subList = list.findAll{ it.itemrefid };
+       def newList = list.findAll{ !it.itemrefid };
+       def arr = [];
+       newList.each { v->
+           arr << v;
+           def sl = subList.findAll{ it.itemrefid == v.objid  };
+           if( sl ) {
+               sl.each { sv ->
+                   sv.acctname = (" "*8) + sv.acctname;
+                   arr << sv;
+               }
+           }
+       }
+       return arr;
+   } 
     
    def itemListModel = [
        fetchList : { o->
            def m = [_schemaname:'jevitem'];
            m.findBy = [jevid: entity.objid ];
            def list = queryService.getList( m ); 
+           
+           def drList = [];
+           def crList = [];
            list.each {
-               if(it.side == "CR" ) it.acctname = (" "*10) + it.acctname;
+               if( it.dr > it.cr ) {
+                   drList << it;
+               }
+               else {
+                   it.acctname = (" "*8) + it.acctname;
+                   crList << it;
+               }
            }
-           def dr = list.sum{ it.dr };
-           def cr = list.sum{ it.cr };
-            
-           list.add( [ acctname: (" "*10) + ' ------- T O T A L S ------- ', dr: dr , cr: cr ] ); 
-           return list; 
+           def dr = drList.sum{ it.dr };
+           def cr = crList.sum{ it.cr };
+           def flist = fixSubList(drList) + fixSubList(crList);
+           flist.add( [ acctname: (" "*8) + ' ------- T O T A L S ------- ', dr: dr , cr: cr ] ); 
+           return flist; 
        }
    ] as BasicListModel;
+   
+   def viewRef() {
+       def op = Inv.lookupOpener(entity.reftype + ":open", [entity: [objid: entity.refid] ] );
+       op.target = "popup";
+       return op;
+   } 
+    
 } 
--- server/rameses-enterprise-accounting-services.jar/views/jevitem.sql
+++ server/rameses-enterprise-accounting-services.jar/views/jevitem.sql
@@ -3,7 +3,7 @@ DROP VIEW IF EXISTS jevitem;
 CREATE VIEW jevitem AS 
 
 SELECT 
-ia.objid, cl.jevid, ia.code AS acctcode, ia.title AS acctname, SUM(dr) AS dr, SUM(cr) AS cr, 'DR' AS side 
+ia.objid, cl.jevid, ia.code AS acctcode, ia.title AS acctname, SUM(dr) AS dr, SUM(cr) AS cr, NULL AS itemrefid 
 FROM cash_treasury_ledger cl
 INNER JOIN itemaccount ia ON ia.objid=cl.`itemacctid` 
 GROUP BY ia.objid,cl.jevid,ia.code,ia.title
@@ -11,7 +11,7 @@ GROUP BY ia.objid,cl.jevid,ia.code,ia.title
 UNION ALL  
 
 SELECT 
-ia.objid, cl.jevid, ia.code AS acctcode, ia.title AS acctname,  SUM(dr) AS dr, SUM(cr) AS cr, 'DR' AS side
+ia.objid, cl.jevid, ia.code AS acctcode, ia.title AS acctname,  SUM(dr) AS dr, SUM(cr) AS cr, NULL AS itemrefid 
 FROM bankaccount_ledger cl
 INNER JOIN itemaccount ia ON ia.objid=cl.itemacctid 
 GROUP BY ia.objid,cl.jevid,ia.code,ia.title
@@ -19,7 +19,7 @@ GROUP BY ia.objid,cl.jevid,ia.code,ia.title
 UNION ALL  
 
 SELECT
-ia.objid, cl.jevid, ia.code AS acctcode, ia.title AS acctname,  SUM(dr) AS dr, SUM(cr) AS cr, 'CR' AS side
+ia.objid, cl.jevid, ia.code AS acctcode, ia.title AS acctname,  SUM(dr) AS dr, SUM(cr) AS cr, NULL AS itemrefid 
 FROM income_ledger cl
 INNER JOIN itemaccount ia ON ia.objid=cl.itemacctid 
 GROUP BY ia.objid,cl.jevid,ia.code,ia.title
@@ -27,7 +27,8 @@ GROUP BY ia.objid,cl.jevid,ia.code,ia.title
 UNION ALL
 
 SELECT
-ia.objid, cl.jevid,  ia.code AS acctcode, ia.title AS acctname,  SUM(dr) AS dr, SUM(cr) AS cr, 'CR' AS side
+ia.objid, cl.jevid,  ia.code AS acctcode, ia.title AS acctname,  SUM(dr) AS dr, SUM(cr) AS cr, ra.objid AS itemrefid
 FROM payable_ledger cl
 INNER JOIN itemaccount ia ON ia.objid=cl.itemacctid 
+INNER JOIN itemaccount ra ON ra.objid=cl.refitemacctid 
 GROUP BY ia.objid,cl.jevid,ia.code,ia.title

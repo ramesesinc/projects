376be6544e5d2c0530a146c21c16a327784c8862
elmonazareno <elmonazareno@gmail.com>
2018-03-09 18:19:39 +0800

    updated billing printing

--- client/plugins/rameses-gov-etracs-waterworks/src/com/rameses/gov/etracs/waterworks/models/BatchBillingModel.groovy
+++ client/plugins/rameses-gov-etracs-waterworks/src/com/rameses/gov/etracs/waterworks/models/BatchBillingModel.groovy
@@ -5,6 +5,8 @@ import com.rameses.rcp.common.*;
 import com.rameses.osiris2.client.*;
 import com.rameses.osiris2.common.*;
 import com.rameses.seti2.models.*;
+import com.rameses.osiris2.report.*;
+import com.rameses.rcp.framework.*;
 import java.text.*;
 
 
@@ -61,6 +63,61 @@ public class BatchBillingModel extends CrudFormModel {
    public void post() {
        if( !MsgBox.confirm("You are about to finalize this transaction for billing. Proceed?")) return;
        batchSvc.post( [objid: entity.objid ]);
+   } 
+    
+   def _printer;
+   def getPrinter() {
+       if(!_printer) {
+           _printer = new TextPrinter();
+           def res = ClientContext.currentContext.classLoader.getResourceAsStream( "com/rameses/gov/etracs/waterworks/reports/billing/waterbilling.rtxt");
+           _printer.setTemplate( res );
+       }
+       return _printer;
+   }
+    
+   def df = new java.text.SimpleDateFormat("yyyy-MM-dd"); 
+   public void printBill() {
+       def mz = [_schemaname: 'waterworks_billing'];
+       mz.findBy = [batchid: entity.objid];
+       mz._limit = 10;
+       def list = queryService.getList(mz);
+       list.each {
+           def p = [:];
+            p.putAll( it );
+            if(p.prevreadingdate ) p.prevreadingdate = df.format( p.prevreadingdate );
+            if(p.readingdate ) p.readingdate = df.format( p.readingdate );
+            p.discrate = 0.0;
+            p.grandtotal = 0;
+            p.subtotal = 0;
+            p.penalty = 0;
+            p.duedate = "";
+            p.penalty = 0;
+            p.classification = p.classification?.objid.toString().padRight(3," ")[0..2]
+            getPrinter().print([ o: p ]);
+       }
+       
+        /*
+        def m = [:];
+        m.name = "test".padLeft(10);
+        m.address = "1111".padLeft(10);
+        m.acctno = "22222".padLeft(10);
+        m.indexno = "33333".padLeft(10);
+        m.classification = "4444".padLeft(10);
+        m.readingdate = "5555".padLeft(10);
+        m.reading = "6666".padLeft(10);
+        m.prevreadingdate = "7777".padLeft(10);
+        m.prevreading = "88888".padLeft(10);
+        m.diffpreprevreading = "9999".padLeft(10);
+        m.amount = "1111111".padLeft(10);
+        m.month = "222222".padLeft(10);
+        m.discrete = "333333".padLeft(10);
+        m.reducebill = "444444".padLeft(10);
+        m.penalty = "555555".padLeft(10);
+        m.totalbill = "66666".padLeft(10);
+        m.duedate = "77777".padLeft(10);
+        m.totalbillarrears = "88888".padLeft(10);
+        getPrinter().print( m );
+        */
    } 
     
 }

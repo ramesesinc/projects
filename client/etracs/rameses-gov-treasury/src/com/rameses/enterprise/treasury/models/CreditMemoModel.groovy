38c2e8c143dc610cf1b18163d347c3853540e630
elmonazareno <elmonazareno@gmail.com>
2018-09-26 21:33:39 +0800

    update credit memo

--- client/etracs/rameses-gov-treasury/src/com/rameses/enterprise/treasury/models/CreditMemoModel.groovy
+++ client/etracs/rameses-gov-treasury/src/com/rameses/enterprise/treasury/models/CreditMemoModel.groovy
@@ -7,18 +7,83 @@ import com.rameses.osiris2.client.*
 import com.rameses.osiris2.common.*
 import java.rmi.server.UID;
 import com.rameses.seti2.models.*;
+import com.rameses.util.*;
+
 
 public class CreditMemoModel extends CrudFormModel {
 
-    def collectionTypes;
-    
-    void afterInit() {
-        def m = [:];
-        m._schemaname = "collectiontype";
-        m.where = ["state='ACTIVE' AND allowcreditmemo = 1"];
-        collectionTypes = queryService.getList(m);
+    void afterCreate() {
+        def collectiontype = null
+        def filter = ["allowcreditmemo = 1 AND orgid IS NULL AND state='ACTIVE' "];
+        def h = { o->
+            collectiontype = o;
+        }
+        Modal.show("collectiontype:lookup", [customFilter:filter, onselect: h]);
+        if(!collectiontype) throw new BreakException();
+        entity.collectiontype = collectiontype;
+        entity.items = [];
+        entity.amount = 0;
     }
     
+    def selectedItem;
+    
+    def getLookupItems() {
+        def p = [:];
+        p.put( "query.collectiontype" , entity.collectiontype );
+        if( entity.collectiontype.fund?.objid ) {
+            p.put("query.fundid", entity.collectiontype.fund?.objid);
+        }
+        p.onselect = { o->
+            selectedItem.item = o;
+            selectedItem.amount = o.remove("amount");
+            selectedItem.remarks = o.remove("remarks");
+        } 
+        return Inv.lookupOpener("cashreceiptitem:lookup",p );
+    }
+
+    void updateBalances() {
+        entity.amount = NumberUtil.round( entity.items.sum{ it.amount } ); 
+        binding.refresh('entity.amount');        
+    }
+    
+    def itemListModel = [
+        fetchList: { o->
+            return entity.items;
+        },
+        onAddItem: {o-> 
+            o.objid = 'CRCI' + new java.rmi.server.UID();
+            entity.items << o;
+            entity.amount += o.amount;
+            updateBalances();
+        },
+        isColumnEditable: { o,name-> 
+            if ( name == 'amount' ) { 
+                def valuetype = o?.item?.valuetype.toString().toUpperCase();
+                if ( valuetype == 'FIXED' ) return false; 
+            }
+            return true; 
+        }, 
+        onColumnUpdate: {o,name-> 
+            if(entity.items) {
+                entity.amount = entity.items.sum{ it.amount };
+                updateBalances();
+            }
+        },
+        onRemoveItem: { o->
+            if(!MsgBox.confirm("You are about to remove this entry. Proceed?")) 
+                return false;
+            entity.items.remove( o );
+            if( entity.items ) {
+                entity.amount = entity.items.sum{ it.amount };
+            }
+            else {
+                entity.amount = 0;
+            }
+            updateBalances();
+            return true;
+        }
+    ] as EditorListModel;
+
     /*
     def submit(){
         if(!entity.items)    
@@ -30,47 +95,6 @@ public class CreditMemoModel extends CrudFormModel {
         return "_close";
     }
 
-    def listHandler = [
-        createItem : { return [parentid:entity.objid] },
-        validate   : { li -> 
-            def item = li.item;
-            if ( ! item.item )
-                throw new Exception('Account is required.');
-            if (item.amount == null || item.amount <= 0.0)
-                throw new Exception('Amount is required.');
-        },
-
-        onAddItem  : { item ->
-            item.objid = 'DCI' + new java.rmi.server.UID();
-            entity.items << item;
-        },
-
-        onRemoveItem : { item ->
-            if (MsgBox.confirm('Delete selected item?')){
-                svc.removeItem(item);
-                entity.items.remove(item);
-                calcAmount();
-                return true;
-            }
-            return false;
-        },
-
-        onCommitItem : {
-            calcAmount();
-        },
-
-        fetchList : { 
-            return entity.items;
-        },
-
-    ] as EditorListModel;
-
-    void calcAmount(){
-        entity.amount = entity.items.amount.sum();
-        if (entity.amount == null)
-            entity.amount = 0.0;
-        binding.refresh('entity.amount');
-    }
     */
     
 }    

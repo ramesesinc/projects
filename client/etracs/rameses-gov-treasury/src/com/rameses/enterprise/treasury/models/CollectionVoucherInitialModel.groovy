963e5e2a670a4e9fd0493a0fdae03850110ec3c9
worgflores <worgflores@gmail.com>
2018-10-01 17:50:53 +0800

    GOV: TREASURY: updated

--- client/etracs/rameses-gov-treasury/src/com/rameses/enterprise/treasury/models/CollectionVoucherInitialModel.groovy
+++ client/etracs/rameses-gov-treasury/src/com/rameses/enterprise/treasury/models/CollectionVoucherInitialModel.groovy
@@ -16,6 +16,8 @@ class CollectionVoucherInitialModel extends CrudListModel {
     def selectedRemittance;
     
     def df = new java.text.SimpleDateFormat("yyyy-MM-dd");
+    def totalAmount = 0.0;
+    def remBalance = 0.0; 
     
     @PropertyChangeListener
     def listener = [
@@ -27,12 +29,15 @@ class CollectionVoucherInitialModel extends CrudListModel {
     
     def remittancelist = null; 
     void buildRemittanceList() {
+        totalAmount = 0.0; 
+        remBalance = 0.0;
         remittancelist = null; 
         if ( !controldate ) return;
         def m = [_schemaname: 'remittance' ];
         m.select = "objid,posted:{CASE WHEN state='POSTED' THEN 1 ELSE 0 END},controlno,controldate,collector.name,amount,totalcash,totalcheck,totalcr,state"
         m.where = [" NOT(state = 'DRAFT') AND collectionvoucherid IS NULL AND controldate =:cdate", [cdate:controldate] ];
         remittancelist = queryService.getList( m );
+        remittancelist.sort{ it.collector?.name.toString() }
     }
     
     void buildDatesList() {
@@ -58,6 +63,8 @@ class CollectionVoucherInitialModel extends CrudListModel {
     def remittanceListHandler = [
         fetchList: { o->
             if( !controldate ) return [];
+            
+            buildTotals(); 
             return remittancelist;
         },
         onOpenItem: {o,col->
@@ -65,19 +72,36 @@ class CollectionVoucherInitialModel extends CrudListModel {
         }
     ] as BasicListModel;
     
+    void buildTotals() {
+        def liqamt = 0.0; 
+        def remamt = 0.0; 
+        remittancelist.each{ 
+            if ( it.posted.toString().matches('1|true')) {
+                liqamt += (it.amount ? it.amount : 0.0); 
+            } else {
+                remamt += (it.amount ? it.amount : 0.0); 
+            }
+        } 
+        remBalance = remamt;
+        totalAmount = liqamt;
+        binding.notifyDepends('totals'); 
+    }
+    
     void viewRemittance() {
         if(!selectedRemittance) throw new Exception("Please select a remittance");
         def h1 = { 
             selectedRemittance.posted = 1;
             selectedRemittance.state = 'POSTED';
             remittanceListHandler.reload();
+            binding.notifyDepends('totals'); 
         };
         def h2 = { 
             buildDatesList();
             binding.refresh('controldate'); 
             
-            buildRemittanceList();
-            remittanceListHandler.reload();
+            buildRemittanceList(); 
+            remittanceListHandler.reload(); 
+            binding.notifyDepends('totals'); 
         } 
         def op = Inv.lookupOpener("remittance:preview", [entity: selectedRemittance, onAccept: h1, onReject: h2 ]);
         op.target = "popup";

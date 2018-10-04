65de174eca301045f0312a617eb26c1d9af23a00
elmonazareno <elmonazareno@gmail.com>
2018-10-04 16:54:57 +0800

    af model

--- client/etracs/rameses-gov-treasury/src/com/rameses/enterprise/treasury/models/AFModel.groovy
+++ client/etracs/rameses-gov-treasury/src/com/rameses/enterprise/treasury/models/AFModel.groovy
@@ -8,6 +8,14 @@ import com.rameses.seti2.models.*;
         
 class AFModel extends CrudFormModel {
 
+    @PropertyChangeListener
+    def listener = [
+        "serial" : { o->
+            if(o==false) entity.serieslength = 0;
+        }
+    ];
+    
+    def serial = false;
     def useTypes = ["collection", "disbursement"];
     def formTypes = [ "serial", "cashticket", "eor" ];
 
@@ -16,5 +24,21 @@ class AFModel extends CrudFormModel {
         entity.serieslength = 0;
         entity.denomination = 0;
     }
+    
+    void afterOpen() {
+        if( entity.formtype == "cashticket" && entity.serieslength > 0 ) {
+            serial = true;
+        }
+    }
+    
+    //
+    void beforeSave(def m ) {
+        if( entity.formtype == "serial" && entity.serieslength == 0 ) {
+            throw new Exception("Please specify a series length");
+        }
+        else if( entity.formtype == "cashticket" && serial==true  && entity.serieslength == 0 ) {
+            throw new Exception("Please specify a series length for cashticket serial");
+        }
+    } 
 
 }        

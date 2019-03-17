3abd9cee1e7b12691420dded9763e5aefc5711f1
Jessie S. Zamora <jzamss@gmail.com>
2019-03-17 12:54:01 +0800

    RPTIS: tracking: update on tracking support

--- resource/etracs/db/v255.03002/v255.03002 script - mssql.sql
+++ resource/etracs/db/v255.03002/v255.03002 script - mssql.sql
@@ -1,3 +1,49 @@
 /* 255-03001 */
 
-alter table rptcertification add properties text;
+alter table rptcertification add properties text
+go 
+
+
+
+alter table rpttracking add dtfiled datetime
+go 
+alter table rpttracking add taxpayer_objid varchar(50)
+go 
+alter table rpttracking add txntype_objid varchar(50)
+go 
+alter table rpttracking add releasedate datetime
+go 
+alter table rpttracking add releasemode varchar(50)
+go 
+alter table rpttracking add receivedby varchar(255)
+go 
+alter table rpttracking add remarks varchar(255)
+go 
+alter table rpttracking add landcount int default 0
+go 
+alter table rpttracking add bldgcount int default 0
+go 
+alter table rpttracking add machcount int default 0
+go 
+alter table rpttracking add planttreecount int default 0
+go 
+alter table rpttracking add misccount int default 0
+go 
+alter table rpttracking add pin varchar(25)
+go 
+alter table rpttracking alter column filetype varchar(50) null
+go 
+	
+
+create index ix_rpttracking_receivedby on rpttracking(receivedby)
+go 
+create index ix_rpttracking_remarks on rpttracking(remarks)
+go 
+create index ix_rpttracking_pin on rpttracking(pin)
+go 
+
+update rpttracking set remarks = msg
+go 
+
+alter table rpttracking drop column msg
+go 
--- resource/etracs/db/v255.03002/v255.03002 script - mysql.sql
+++ resource/etracs/db/v255.03002/v255.03002 script - mysql.sql
@@ -1,2 +1,37 @@
 /* 255-03001 */
 alter table rptcertification add properties text;
+
+
+alter table rpttracking 
+	add dtfiled datetime,
+	add pin varchar(25),
+	add taxpayer_objid varchar(50),
+	add txntype_objid varchar(50),
+	add releasedate datetime,
+	add releasemode varchar(50),
+	add receivedby varchar(255),
+	add remarks varchar(255),
+	add landcount int default 0,
+	add bldgcount int default 0,
+	add machcount int default 0,
+	add planttreecount int default 0,
+	add misccount int default 0
+;
+alter table rpttracking modify column filetype varchar(50) null
+;
+	
+
+create index ix_rpttracking_receivedby on rpttracking(receivedby)
+;
+create index ix_rpttracking_remarks on rpttracking(remarks)
+;
+create index ix_rpttracking_pin on rpttracking(pin)
+;
+
+update rpttracking set remarks = msg
+;
+
+alter table rpttracking drop column msg
+;	
+
+	

4f19c0e4ccf9ed9d5953fff9164c4f7cc810326d
Jessie S. Zamora <jzamss@gmail.com>
2019-03-24 13:16:26 +0800

    LANDTAX: taxclearance update

--- client/etracs/rameses-gov-etracs-rptis-landtax-report/src/com/rameses/gov/etracs/landtax/report/models/TaxClearanceModel.groovy
+++ client/etracs/rameses-gov-etracs-rptis-landtax-report/src/com/rameses/gov/etracs/landtax/report/models/TaxClearanceModel.groovy
@@ -218,24 +218,24 @@ class TaxClearanceModel
     void validateLedger(ledger) {
         def msg = '';
         
-        if (!equalTaxpayerId(ledger)) {
-            if (!entity.items) {
-                msg = 'The Owner of the selected ledger is not equal to Taxpayer.\n';
-                msg += 'Proceeding will replace the Taxpayer that you selected with the ledger owner.\n\nContinue?'
-                if (MsgBox.confirm(msg)) {
-                    taxpayer = null;
-                    initTaxpayer(ledger.taxpayer)
-                } else {
-                    return;
-                }
-            } else  {
-                msg = 'The selected ledger Owner account #' + ledger.taxpayer.entityno + ' is not equal to the Taxpayer account #' + entity.taxpayer.entityno + '.\n';
-                msg += 'Verify that this ledger is currently owned by this taxpayer and if this \n';
-                msg += 'is the case, kindly request the Assessor to modify the Property Owner \n';
-                msg += 'of the FAAS before including this ledger in the certification.';
-                throw new Exception(msg);
-            }
-        }
+//        if (!equalTaxpayerId(ledger)) {
+//            if (!entity.items) {
+//                msg = 'The Owner of the selected ledger is not equal to Taxpayer.\n';
+//                msg += 'Proceeding will replace the Taxpayer that you selected with the ledger owner.\n\nContinue?'
+//                if (MsgBox.confirm(msg)) {
+//                    taxpayer = null;
+//                    initTaxpayer(ledger.taxpayer)
+//                } else {
+//                    return;
+//                }
+//            } else  {
+//                msg = 'The selected ledger Owner account #' + ledger.taxpayer.entityno + ' is not equal to the Taxpayer account #' + entity.taxpayer.entityno + '.\n';
+//                msg += 'Verify that this ledger is currently owned by this taxpayer and if this \n';
+//                msg += 'is the case, kindly request the Assessor to modify the Property Owner \n';
+//                msg += 'of the FAAS before including this ledger in the certification.';
+//                throw new Exception(msg);
+//            }
+//        }
         
         validateState(ledger);
         

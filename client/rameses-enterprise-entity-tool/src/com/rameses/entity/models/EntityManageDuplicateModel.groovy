7dc0761b0b40108ecea1964dfa08389fbdc3e014
Jessie S. Zamora <jzamss@gmail.com>
2019-09-27 15:28:48 +0800

    ENTITY: duplicate management tool

---
+++ client/rameses-enterprise-entity-tool/src/com/rameses/entity/models/EntityManageDuplicateModel.groovy
@@ -0,0 +1,88 @@
+package com.rameses.entity.models;
+
+import com.rameses.rcp.annotations.*;
+import com.rameses.rcp.common.*;
+import com.rameses.osiris2.client.*;
+import com.rameses.osiris2.common.*;
+
+class EntityRemoveDuplicateModel {
+    @Service('PersistenceService')
+    def persistence;
+    
+    @Service('QueryService')
+    def qrySvc 
+    
+    String title = 'Manage Duplicate Entity';
+    
+    def items = [];
+    def searchtext;
+    def selectedItem;
+    
+    void refresh() {
+        search();
+    }
+    
+    void search() {
+        items = []
+        if (searchtext) {
+            def param = [_schemaname: 'entity'];
+            param.select = 'objid, name, address_text, type, _isbpls';
+            param.where = ['state = :state and name like :name', [state:'ACTIVE', name: searchtext+'%']];
+            param.orderBy = 'name';
+            items = qrySvc.getList(param);
+        }
+        listHandler.reload();
+    }
+    
+    void deleteEntity(entity) {
+        try {
+            def e = [_schemaname: 'entity' + entity.type.toLowerCase(), objid: entity.objid];
+            persistence.removeEntity(e);
+            items.remove(entity);
+        } catch (e) {
+            MsgBox.alert('Unable to delete ' + entity.name + '.\nIt is currently referenced by a transaction.');
+        }
+    }
+    
+    void delete() {
+        def selecteditems = listHandler.selectedValue;
+        if (!selecteditems) return;
+        if (MsgBox.confirm('Delete selected items?')) {
+            selecteditems.each {
+                deleteEntity(it);
+            }
+            listHandler.reload();
+        }
+    }
+    
+    void deactivateEntity(entity) {
+        def e = [_schemaname: 'entity' + entity.type.toLowerCase(), objid: entity.objid, state:'INACTIVE'];
+        persistence.update(e);
+        items.remove(entity);
+    }
+    
+    void deactivate() {
+        def selecteditems = listHandler.selectedValue;
+        if (!selecteditems) return;
+        if (MsgBox.confirm('Deactivate selected items?')) {
+            selecteditems.each {
+                deactivateEntity(it);
+            }
+            listHandler.reload();
+        }
+    }
+    
+    def listHandler = [
+        getRows : { items.size() },
+        fetchList: { items },
+        isMultiSelect: { true },
+        onOpenItem: {item, col ->
+            def opener = 'entity' + item.type.toLowerCase() + ':open';
+            def inv = Inv.lookupOpener(opener, [entity: item]);
+            inv.target = 'popup';
+            return inv;
+        }
+    ] as BasicListModel;
+    
+    
+}

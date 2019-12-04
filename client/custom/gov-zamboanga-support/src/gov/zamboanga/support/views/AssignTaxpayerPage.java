778e5a89d712d771910eb3aff650d013ebaded6d
Jessie S. Zamora <jzamss@gmail.com>
2019-12-04 08:47:38 +0800

    CUSTOM: zamboanga: refactory support project

--- client/custom/gov-zamboanga-support/src/gov/zamboanga/support/views/AssignTaxpayerPage.java
+++ client/custom/gov-zamboanga-support/src/gov/zamboanga/support/views/AssignTaxpayerPage.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package gov.zamboanga.landtax.views;
+package gov.zamboanga.support.views;
 
 /**
  *

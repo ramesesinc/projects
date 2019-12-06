f42f5f2142bcf5411ed7cd08a3ffba787235ced3
Jessie S. Zamora <jzamss@gmail.com>
2019-12-06 16:31:11 +0800

    CUSTOM: zamboanga: support: assign property owner update

--- client/custom/gov-zamboanga-support/src/gov/zamboanga/support/models/AssignTaxpayerModel.groovy
+++ client/custom/gov-zamboanga-support/src/gov/zamboanga/support/models/AssignTaxpayerModel.groovy
@@ -3,8 +3,12 @@ package gov.zamboanga.landtax.models;
 import com.rameses.rcp.annotations.*;
 import com.rameses.rcp.common.*;
 import com.rameses.osiris2.common.*;
+import com.rameses.osiris2.client.*;
 
 class AssignTaxpayerModel {
+    @Binding
+    def binding;
+    
     @Service('ZamboangaLandTaxService')
     def svc;
     
@@ -15,6 +19,18 @@ class AssignTaxpayerModel {
     def selectedItem;
     def onassign = {};
     
+    def onSaveEntity = {
+        taxpayer = it;
+        binding.refresh('taxpayer.*');
+    }
+    
+    def createEntity(param) {
+        def opener = 'entity' + param.properties.tag + ':create';
+        def inv = Inv.lookupOpener(opener, [onSaveHandler: onSaveEntity]);
+        inv.target = 'popup';
+        return inv;
+    }
+        
     def assign() {
         if (!taxpayer) {
             throw new Exception('A new taxpayer should be selected.');

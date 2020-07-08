b001a65e8f78d14cd82fe8b10c7dcfe1cc1849a0
worgflores <worgflores@gmail.com>
2020-07-08 13:42:14 +0800

    BPLS: REPORT: updated

--- client/etracs/rameses-gov-etracs-bpls-reports/src/com/rameses/gov/etracs/bpls/reports/models/DTICompetitivenessIndexReportModel.groovy
+++ client/etracs/rameses-gov-etracs-bpls-reports/src/com/rameses/gov/etracs/bpls/reports/models/DTICompetitivenessIndexReportModel.groovy
@@ -8,6 +8,9 @@ import com.rameses.osiris2.reports.*;
 
 public class DTICompetitivenessIndexReportModel extends AsyncReportController {
 
+    @Script('ReportPeriod') 
+    def periodUtil; 
+
     @Service('BPReportDTICompetitivenessIndexService')
     def svc; 
 
@@ -18,14 +21,21 @@ public class DTICompetitivenessIndexReportModel extends AsyncReportController {
     String reportpath = 'com/rameses/gov/etracs/bpls/reports/'; 
 
     def permitTypes;
+    def periodTypes;
     def templates = []; 
+    def daterefs = [];
     
     def formControl = [
         getControlList: { 
             return [
                 [type:'combo', caption:'Template', name:'entity.template', required:true, items:'templates',  expression:'#{item.title}', preferredSize:'0,20', captionWidth:100], 
                 [type:'combo', caption:'Permit Type', name:'entity.permittype', required:true, items:'permitTypes',  expression:'#{item.title}', preferredSize:'0,20', captionWidth:100], 
-                [type:'integer', caption:'Year', name:'entity.year', required:true, depends:'entity.period', preferredSize:'100,20', captionWidth:100 ] 
+                [type:'combo', caption:'Date Ref', name:'entity.dateref', required:true, items:'daterefs',  expression:'#{item.title}', preferredSize:'0,20', captionWidth:100], 
+
+                [type:'combo', caption:'Period', name:'entity.period', required:true, items:'periodTypes',  expression:'#{item.title}', preferredSize:'100,20', captionWidth:100],
+                [type:'integer', caption:'Year', name:'entity.year', required:true, depends:'entity.period', visibleWhen:'#{ entity.period.type.matches("yearly|quarterly|monthly")==true }', preferredSize:'100,20', captionWidth:100 ],
+                [type:'combo', caption:'Quarter', name:'entity.qtr', required:true, items:'periodUtil.quarters', depends:'entity.period', visibleWhen:'#{ entity.period.type=="quarterly" }', preferredSize:'100,20', captionWidth:100],
+                [type:'combo', caption:'Month', name:'entity.month', required:true, items:'periodUtil.months', expression:'#{item.title}', depends:'entity.period', visibleWhen:'#{ entity.period.type=="monthly" }', preferredSize:'100,20', captionWidth:100],
             ]; 
         } 
     ] as FormPanelModel; 
@@ -33,12 +43,21 @@ public class DTICompetitivenessIndexReportModel extends AsyncReportController {
     def initReport() {
         def outcome = super.initReport(); 
         permitTypes = permitTypeSvc.getList(); 
-        entity.permittype = (permitTypes? permitTypes[0] : null); 
+        entity.permittype = (permitTypes? permitTypes.first() : null); 
+        
+        periodTypes = periodUtil.types.findAll{( it.type.matches('yearly|quarterly|monthly') )} 
+        entity.period = (periodTypes ? periodTypes.first() : null);
         
         templates = [
             [objid: 'default', title: 'Default', report: reportpath + 'dti_competitiveness_index.jasper'] 
         ]; 
         entity.template = templates.first(); 
+        
+        daterefs = [
+            [objid: 'date-applied', title: 'Application Date'],
+            [objid: 'date-released', title: 'Released Date']
+        ]; 
+        entity.dateref = daterefs.first(); 
         return outcome; 
     } 
     
@@ -50,6 +69,10 @@ public class DTICompetitivenessIndexReportModel extends AsyncReportController {
     }
     
     void buildReportData(entity, asyncHandler) { 
+        def m = periodUtil.build( entity.period.type, entity ); 
+        entity.startdate = m.startdate;
+        entity.enddate = m.enddate; 
+        
         svc.getReport( entity, asyncHandler );
     }
     
